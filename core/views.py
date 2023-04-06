8a166e6d75fa9c5f5ee3a042124dda63e12610b1
johnpauljpc <jpcwork081@gmail.com>
2023-04-06 17:53:12 +0100

    templates configuration

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
 
 # Create your views here.
+class IndexView(View):
+    def get(self, request):
+        return render(request, 'core/index.html')
+    
+    def post(self, request):
+        return HttpResponse("this is a post request")
