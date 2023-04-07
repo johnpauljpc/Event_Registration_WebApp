1f7451b857d82144256ef5cf7ec821755d41122a
johnpauljpc <jpcwork081@gmail.com>
2023-04-07 21:04:20 +0100

    tweaked

--- core/views.py
+++ core/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from django.contrib.auth import get_user_model
 
 # Create your views here.
 class IndexView(View):
     def get(self, request):
-        return render(request, 'core/index.html')
+        users = get_user_model()
+        Users = users.objects.all()
+        context = {
+            'users':Users
+        }
+        return render(request, 'core/index.html', context)
     
     def post(self, request):
         return HttpResponse("this is a post request")
