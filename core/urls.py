bfb72a82b80da3d09515b16be57218045d6d83ac
johnpauljpc <jpcwork081@gmail.com>
2023-04-10 19:57:35 +0100

    event registration confirmation implemented

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('event/<int:pk>/', EventView.as_view(), name='event')
+    path('event/<int:pk>/', EventView.as_view(), name='event'),
+    path('confirm-registration/event/<int:pk>/', Confirm_Event_Registration.as_view(), name="confirm-event")
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.http import HttpResponse
+from django.contrib import messages
 from django.contrib.auth import get_user_model
 from .models import Event
 
@@ -25,8 +26,43 @@ class EventView(View):
         try:
             event = Event.objects.get(pk=pk)
         except Event.DoesNotExist:
-            return HttpResponse("event not found")
+            messages.warning(request, "event not found")
+            return redirect(request.META.get("HTTP_REFERER", '/'))
+            # return render(request, 'core/event.html', context)
         context = {
             'event':event
         }
         return render(request, 'core/event.html', context)
+
+class Confirm_Event_Registration(View):
+    def get(self, request, pk):
+        try:
+            event = Event.objects.get(pk=pk)
+        except Event.DoesNotExist:
+            return HttpResponse("404, not found!")
+        context = {
+            'event':event
+        }
+
+        return render(request, 'core/event_confirmation.html', context)
+    
+    def post(self, request, pk):
+        try:
+            user = request.user
+            event = Event.objects.get(pk=pk)
+
+            #check if user is already a participant
+            if user in event.participants.all():
+                messages.info(request, 'You have been added already!')
+                return redirect('event', event.pk)
+            else:
+                event.participants.add(request.user)
+                messages.success(request, f'{request.user}, You have been added as a participant of this event')
+                return redirect('event', event.pk)
+        except Event.DoesNotExist:
+            return HttpResponse("error, not found!")
+        context = {
+            'event':event
+        }
+
+        return render(request, 'core/event_confirmation.html', context)
