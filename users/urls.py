f38c0184802aac27f34a3ffd32a5e1f6f4e9f775
johnpauljpc <jpcwork081@gmail.com>
2023-04-11 05:30:07 +0100

    navbar and user  profile page

---
+++ users/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('profile/<int:pk>/', User_Profile.as_view(), name='user-profile')
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+from .models import User
 
 # Create your views here.
+class User_Profile(View):
+    def get(self, request, pk):
+        user = User.objects.filter(id = pk).first()
+
+        context = {
+            'user':user
+        }
+        return render(request, 'users/user_profile.html', context)
