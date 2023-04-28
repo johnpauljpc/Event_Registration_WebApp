9d0c2ee3e5802eaad3020fb1b3e4952bb7e5660e
johnpauljpc <jpcwork081@gmail.com>
2023-04-28 05:30:01 +0100

    login logic view completed

--- users/views.py
+++ users/views.py
@@ -1,14 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import View
 from .models import User
 from core.models import Event
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 # Create your views here.
 def loginPage(request):
+    
+    if request.method == 'POST':
+        user = authenticate(email = request.POST['email'], password = request.POST['password'])
+        if user is not None:
+            login(request, user)
+            messages.success(request, f"Welcome <b>{user}</b>!")
+            return redirect('index')
+        messages.error(request, "credentials provided is wrong!")
     context = {
         'page':"login"
-    }
+    }    
     return render(request, "users/login_register.html", context)
 
 def signUpPage(request):
